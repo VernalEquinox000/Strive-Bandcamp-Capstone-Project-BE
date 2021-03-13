40bd04f5e0dabb9dfa615fc75a8410d276781a3f
VernalEquinox <pieroapruzzese@gmail.com>
2021-03-13 23:06:39 +0100

    fanModel

---
+++ src/models/fanModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = moongose.Schema;
+
+const FanSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: "Email address is required",
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "That email address doesn’t look right.",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    //password must be of 3 chars
+    username: {
+      type: String,
+      required: "Please choose your username",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = FanSchema;
