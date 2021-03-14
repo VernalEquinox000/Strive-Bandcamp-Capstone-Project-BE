e9ae1d67b9f658df8094c24b94e6005dbef8d930
VernalEquinox <pieroapruzzese@gmail.com>
2021-03-14 23:56:56 +0100

    auth Middleware

--- src/middleware/authTools.js
+++ src/middleware/authTools.js
@@ -3,4 +3,52 @@ const mongoose = require("mongoose");
 const FanSchema = require("../models/fanModel");
 const FanModel = mongoose.model("Fan", FanSchema);
 
+const generateAccessToken = (payload) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      process.env.JWT.SECRET,
+      { expiresIn: "15m" },
+      (err, token) => {
+        if (err) rej(err);
+        res(token);
+      }
+    )
+  );
+
+verifyAccessToken = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        rej(err);
+      }
+      res(decoded);
+    })
+  );
+
+const generateRefreshToken = (payload) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      process.env.JWT_REFRESH.SECRET,
+      { expiresIn: "15m" },
+      (err, token) => {
+        if (err) rej(err);
+        res(token);
+      }
+    )
+  );
+
+verifyRefreshToken = (token) =>
+  new Promise((res, rej) =>
+    jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        rej(err);
+      }
+      res(decoded);
+    })
+  );
+
 module.exports = {};
