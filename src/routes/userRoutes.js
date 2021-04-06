8c8e05d4e6e746f3f6d587d99e0814cd19d92044
VernalEquinox <pieroapruzzese@gmail.com>
2021-04-06 11:16:22 +0200

    minor changes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,6 +10,8 @@ const {
   googleAuth,
   cloudMulter,
   addProfilePic,
+  addHeaderPic,
+  addBackgroundPic,
   getUserById,
 } = require("../controllers/userController");
 const { authorize } = require("../middleware/authMiddleware");
@@ -27,7 +29,14 @@ const routes = (app) => {
     .delete(authorize, deleteUser);
   app
     .route("/users/me/profilePic")
-    .post(authorize, cloudMulter.single("picture"), addProfilePic);
+    .post(authorize, cloudMulter.single("profilePic"), addProfilePic);
+  app
+    .route("/users/me/headerPic")
+    .post(authorize, cloudMulter.single("headerPic"), addHeaderPic);
+  app
+    .route("/users/me/backgroundPic")
+    .post(authorize, cloudMulter.single("backgroundPic"), addProfilePic);
+
   app.route("/users/refreshToken").post(refreshToken);
   app
     .route("/googleLogin")
